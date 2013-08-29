1ea36b8366d204bd9a8fb41b17e70fbc3e325e79
Ross Brown <ross@cremalab.com>
2013-08-29 09:29:46 -0500

    something probably useful

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -39,6 +39,6 @@ class IdeasController < ApplicationController
 
   private
     def idea_params
-      params.require(:idea).permit(:title, :when, :user_id)
+      params.require(:idea).permit(:title, :when, :user_id, :description)
     end
 end
