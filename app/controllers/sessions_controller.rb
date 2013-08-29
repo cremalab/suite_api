35deb266ee0c85f12e78ca390bf11da03c75001b
Ross Brown <ross@cremalab.com>
2013-08-29 12:34:53 -0500

    return logged_in on session response

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password], params[:remember_me])
     if @user
+      @logged_in = current_user == @user
       render "users/show", status: 200
     else
       render :json => ['Invalid email or password'], status: 401
