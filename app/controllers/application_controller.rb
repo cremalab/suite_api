b9d55692bd35efccfd4909cbd74a373ab96eba8b
Ross Brown <ross@cremalab.com>
2013-08-29 19:43:08 -0500

    Using request headers for authentication. Ahhhh, much better

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null_session
+
   private
 
     def ensure_authenticated
-      if params[:auth] && params[:auth][:user_id] && params[:auth][:access_token]
-        user = User.find(params[:auth][:user_id])
+      access_token = request.headers['HTTP_X_ACCESS_TOKEN']
+      user_id      = request.headers['HTTP_X_USER_ID']
+      if access_token && user_id
         api_key = ApiKey.where(
-          access_token: params[:auth][:access_token],
-          user_id: params[:auth][:access_token]
+          access_token: access_token,
+          user_id: user_id
         )
-        head :unauthorized unless api_key
+        if api_key
+          current_user = access_token
+        else
+          head :unauthorized unless api_key
+        end
       else
         head :unauthorized
       end
