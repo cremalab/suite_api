825da03b195b7d4455a90077ad2ffdabdcb46c89
Ross Brown <ross@cremalab.com>
2013-09-10 18:37:43 -0500

    adding create action to VotingRightController

--- app/controllers/voting_rights_controller.rb
+++ app/controllers/voting_rights_controller.rb
@@ -1,6 +1,15 @@
 class VotingRightsController < ApplicationController
   before_action :ensure_authenticated
 
+  def create
+    @voting_right = VotingRight.new(voting_right_params)
+    if @voting_right.save
+      render :show, status: :ok
+    else
+      render :json => @voting_right.errors.full_messages, status: 422
+    end
+  end
+
   def destroy
     @voting_right = VotingRight.find(params[:id])
     if @voting_right.destroy
@@ -14,4 +23,10 @@ class VotingRightsController < ApplicationController
     @voting_right = VotingRight.find(params[:id])
     render :show, status: :ok
   end
+
+private
+  def voting_right_params
+    params.require(:voting_right).permit(:idea_thread_id, :user_id)
+  end
+
 end
