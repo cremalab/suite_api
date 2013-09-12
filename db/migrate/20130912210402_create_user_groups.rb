ca441213a20b2e71771ed8592732ba495b7b5c41
matao <mattowens11@gmail.com>
2013-09-12 16:38:41 -0500

    created migrations

--- db/migrate/20130912210402_create_user_groups.rb
+++ db/migrate/20130912210402_create_user_groups.rb
@@ -1,6 +1,9 @@
 class CreateUserGroups < ActiveRecord::Migration
   def change
     create_table :user_groups do |t|
+      t.references :users, index: true
+      t.references :users, index: true
+
 
       t.timestamps
     end
