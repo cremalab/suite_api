c1dd74a62153aef15bfa07bc329f41f78261bbbe
matao <mattowens11@gmail.com>
2013-09-20 15:51:27 -0500

    Add scaffold for group controller

---
+++ test/controllers/groups_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class GroupsControllerTest < ActionController::TestCase
+  test "create" do
+    assert false
+  end
+
+  test "show" do
+    assert false
+  end
+
+  test "update" do
+    assert false
+  end
+
+  test "destroy" do
+    assert false
+  end
+
+
+end
