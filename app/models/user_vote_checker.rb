6af42b769233ce634be3a3b6bac40aa4f980efe6
Ross Brown <ross@cremalab.com>
2013-09-23 16:33:37 -0500

    change UserVoteChecker to destroy all other user votes. closes #55

--- app/models/user_vote_checker.rb
+++ app/models/user_vote_checker.rb
@@ -4,17 +4,16 @@ class UserVoteChecker
     @thread = thread
   end
 
-  def get_existing_user_vote(new_vote)
-    vote = @thread.votes.where(user_id: @user.id).where.not(id: new_vote.id).first
-    return vote
+  def get_existing_user_votes(new_vote)
+    votes = @thread.votes.where(user_id: @user.id).where.not(id: new_vote.id)
+    return votes
   end
 
   def save(new_vote)
-    existing_user_vote = get_existing_user_vote(new_vote)
-
     if new_vote.save
-      if existing_user_vote
-        if existing_user_vote.destroy
+      existing_user_vote = get_existing_user_votes(new_vote)
+      if existing_user_vote.count > 0
+        if existing_user_vote.destroy_all
           return true
         else
           return false
