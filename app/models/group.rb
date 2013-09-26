acbbf02361cc84a00c55501cfff8bf91aa5886f1
matao <mattowens11@gmail.com>
2013-09-26 13:45:34 -0500

    Initial Comments

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,10 @@
 class Group < ActiveRecord::Base
+  #Relationships
   has_many :memberships, foreign_key: "group_id"
   has_many :users, through: :memberships
   belongs_to :owner, class_name: 'User'
 
   accepts_nested_attributes_for :memberships
+
+  #Validation
 end
