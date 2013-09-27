772b96b0a5b32fe95db13dc99bf6b21aaed6e034
matao <mattowens11@gmail.com>
2013-09-27 11:10:00 -0500

    Wrote groups test

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -4,4 +4,21 @@ class GroupTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "validations" do
+    #Setup Attributes
+    new_group_attr = {name: "Yellow Submarine", owner_id: 1}
+
+    #With all necessary values
+    new_group = Group.new(new_group_attr)
+    assert new_group.valid?
+
+    #Without Name
+    new_group.name = nil
+    refute new_group.valid?
+    new_group = Group.new(new_group_attr)
+
+    #Without owner_id
+    new_group.owner_id = nil
+    refute new_group.valid?
+  end
 end
