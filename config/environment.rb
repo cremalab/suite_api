79c5d0403ddcfef26ae39002b417e5f78e600987
matao <mattowens11@gmail.com>
2013-10-10 16:28:20 -0500

    Scaffolded the basic files

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+#Needs to be filled out
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'support@cremalab.com',
+  :password => '',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 SuiteApi::Application.initialize!
