36840bf226e5b71eefd32bf80bc5b949d68288d1
Ross Brown <ross@cremalab.com>
2013-10-22 16:38:11 -0500

    aliasing owner_id to user_id in Activity json. #92

--- app/serializers/public_activity/activity_serializer.rb
+++ app/serializers/public_activity/activity_serializer.rb
@@ -1,10 +1,16 @@
 class PublicActivity::ActivitySerializer < ActiveModel::Serializer
   attributes  :id, :created_at, :trackable_id, :trackable_type,
-              :recipient_type, :recipient_id, :key, :model_name
+              :recipient_type, :recipient_id, :key, :model_name,
+              :user_id
 
   has_one :owner
 
   def model_name
     "Activity"
   end
+
+  def user_id
+    object.owner_id
+  end
+
 end
