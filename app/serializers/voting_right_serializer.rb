8634fd9540303ae702f43e0c87f4b18b392d29fd
Ross Brown <ross@cremalab.com>
2013-10-24 11:31:47 -0500

    fixing VotingRightsSerializer

--- app/serializers/voting_right_serializer.rb
+++ app/serializers/voting_right_serializer.rb
@@ -7,14 +7,14 @@
 #
 #
 class VotingRightSerializer < ActiveModel::Serializer
-  attributes  :id, :idea_thread_id, :user_id, :created_at, :autocomplete_value
+  attributes  :id, :idea_thread_id, :user_id, :created_at, :autocomplete_value,
               :autocomplete_search
 
   def autocomplete_value
-    self.user_id
+    object.user_id
   end
 
   def autocomplete_search
-    self.voter.display_name
+    object.voter.display_name
   end
 end
