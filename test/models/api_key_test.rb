5d43f7aab80034a12eb4d1ff37bf0bfb33f5ab39
matao <mattowens11@gmail.com>
2013-10-25 13:28:19 -0500

    Added api tests

--- test/models/api_key_test.rb
+++ test/models/api_key_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class ApiKeyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "generate_access_token" do
+    api_key = ApiKey.create()
+    refute_nil api_key.access_token
+    assert_equal api_key.access_token.length, 32
+  end
 end
