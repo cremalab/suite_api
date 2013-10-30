fb015a307fec37b9125c84af55240e5a4529a304
Ross Brown <ross@cremalab.com>
2013-10-30 14:41:59 -0500

    update notification tests. #104

--- test/models/notifier_test.rb
+++ test/models/notifier_test.rb
@@ -2,26 +2,37 @@ require 'test_helper'
 
 class NotifierTest < ActiveSupport::TestCase
 
+  def setup
+    @user = users(:rob)
+    @thread = idea_threads(:lunch)
+    @idea = ideas(:sandwich)
+    @vote = votes(:rob_vote_sandwich)
+  end
+
   test "new_thread" do
-    Notifier.new_thread(["mattowens11@gmail.com"]).deliver
+    thread = idea_threads()
+    Notifier.new_thread(["mattowens11@gmail.com"], @thread).deliver
     mail = ActionMailer::Base.deliveries.last
 
     assert_includes mail.body, "Put on your thinkin' caps!"
   end
 
   test "new_idea" do
-    Notifier.new_idea(["mattowens11@gmail.com"]).deliver
+    Notifier.new_idea(["mattowens11@gmail.com"], @idea).deliver
     mail = ActionMailer::Base.deliveries.last
 
-    assert_includes mail.body, "New idea! Or updated idea!"
+    assert_includes mail.body, "Rob LaFeve"
+    assert_includes mail.body, "Lunch:"
+    assert_includes mail.body, "Chicken Salad Sandwiches at Sylvias"
 
   end
 
   test "new_vote" do
-    Notifier.new_vote(["mattowens11@gmail.com"]).deliver
+    Notifier.new_vote(["mattowens11@gmail.com"], @vote).deliver
     mail = ActionMailer::Base.deliveries.last
 
-    assert_includes mail.body, "New vote!"
+    assert_includes mail.body, "Rob LaFeve"
+    assert_includes mail.body, "Chicken Salad Sandwiches at Sylvias"
 
   end
 
