93ae20ea121c8a8662f1b6eff03b3707ce6b36e9
matao <mattowens11@gmail.com>
2013-10-31 17:13:01 -0500

    Wrote code to get average, variance, and standard deviation

--- app/models/analytics/base.rb
+++ app/models/analytics/base.rb
@@ -1,2 +1,30 @@
+include Math
 class Analytics::Base < ActiveRecord::Base
+
+  #Segement is 5 minutes currently
+  def self.average_per_segment(column)
+    values = self.select(column).all
+    amount = values.length
+    sum = 0
+    values.each do |value|
+      sum += value[column]
+    end
+    avg = sum.to_f/amount
+  end
+
+  def self.var_per_segment(column)
+    avg = Analytics::Base.average(column)
+    values = self.select(column).all
+    ar = values.map{|v| (v.idea_thread_creates - avg)**2}
+    sum = 0
+    ar.each do |x|
+      sum += x
+    end
+    sum/avg
+  end
+
+  def self.std_dev_per_segment(column)
+    var = Analytics::Base.var_per_segment(column)
+    sqrt(var)
+  end
 end
--- test/models/analytics/base_test.rb
+++ test/models/analytics/base_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class Analytics::BaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "self.average" do
+    avg = Analytics::Base.average(:idea_thread_creates)
+
+    assert_equal avg, 20
+
+  end
+
+  test "self.var_per_segment" do
+    var = Analytics::Base.var_per_segment(:idea_thread_creates)
+
+    assert 1.2, var
+  end
+
+  test "self.std_dev_per_segment" do
+    std_dev = Analytics::Base.std_dev_per_segment(:idea_thread_creates)
+
+    assert_in_delta 1.09, std_dev, 0.01
+
+  end
+
 end
