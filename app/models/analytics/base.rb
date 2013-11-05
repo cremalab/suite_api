9daae208b5f01255df18475035e1802ff003bfed
matao <mattowens11@gmail.com>
2013-11-05 16:30:59 -0600

    Finished the segment functions.

--- app/models/analytics/base.rb
+++ app/models/analytics/base.rb
@@ -3,10 +3,7 @@ class Analytics::Base < ActiveRecord::Base
 
   #Segement is 5 minutes currently
   def self.average_per_segment(column, s_time = Time.new(2000), e_time = Time.now)
-    p s_time
-    p e_time
-    values = self.select(column).where(created_at: s_time..e_time)
-    p values
+    values = self.select(column).where(start_time: s_time..e_time)
     amount = values.length
     sum = 0
     values.each do |value|
@@ -15,19 +12,20 @@ class Analytics::Base < ActiveRecord::Base
     avg = sum.to_f/amount
   end
 
-  def self.var_per_segment(column)
-    avg = Analytics::Base.average(column)
-    values = self.select(column).all
+  def self.var_per_segment(column, s_time = Time.new(2000), e_time = Time.now)
+    avg = Analytics::Base.average_per_segment(column, s_time, e_time)
+    values = self.select(column).where(start_time: s_time..e_time)
     ar = values.map{|v| (v.idea_thread_creates - avg)**2}
     sum = 0
     ar.each do |x|
       sum += x
     end
+
     sum/avg
   end
 
-  def self.std_dev_per_segment(column)
-    var = Analytics::Base.var_per_segment(column)
+  def self.std_dev_per_segment(column, s_time = Time.new(2000), e_time = Time.now)
+    var = Analytics::Base.var_per_segment(column, s_time, e_time)
     sqrt(var)
   end
 end
--- test/models/analytics/base_test.rb
+++ test/models/analytics/base_test.rb
@@ -11,7 +11,7 @@ class Analytics::BaseTest < ActiveSupport::TestCase
   test "self.var_per_segment" do
     var = Analytics::Base.var_per_segment(:idea_thread_creates)
 
-    assert 1.2, var
+    assert_equal 1.2, var
   end
 
   test "self.std_dev_per_segment" do
@@ -22,20 +22,28 @@ class Analytics::BaseTest < ActiveSupport::TestCase
   end
 
   test "self.average_per_segment with time" do
+    avg = Analytics::Base.average_per_segment(:idea_thread_creates,
+                                              "2013-10-31 16:00",
+                                              "2013-10-31 16:05")
 
-    assert false
+    assert_equal avg, 20
 
   end
 
   test "self.var_per_segment with time" do
-
-    assert false
+    var = Analytics::Base.var_per_segment(:idea_thread_creates,
+                                          "2013-10-31 16:00",
+                                          "2013-10-31 16:05")
+    assert_equal 0.1, var
 
   end
 
   test "self.std_dev_per_segment with time" do
+    std_dev = Analytics::Base.std_dev_per_segment(:idea_thread_creates,
+                                                  "2013-10-31 16:00",
+                                                  "2013-10-31 16:05")
 
-    assert false
+    assert_in_delta 0.31, std_dev, 0.01
 
   end
 
