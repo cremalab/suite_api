2a83313df4ded4037859f51b0788074f23bb7d9a
matao <mattowens11@gmail.com>
2013-12-03 15:20:49 -0600

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20131106163738) do
-=======
-ActiveRecord::Schema.define(version: 20131031192806) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +41,6 @@ ActiveRecord::Schema.define(version: 20131031192806) do
     t.integer  "idea_updates"
     t.integer  "idea_deletes"
     t.integer  "vote_creates"
-<<<<<<< HEAD
-    t.integer  "vote_updates"
-=======
->>>>>>> master
     t.integer  "vote_deletes"
     t.integer  "comment_creates"
     t.integer  "comment_updates"
